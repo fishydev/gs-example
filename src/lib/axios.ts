60ceeaed7cd278ea46d48ca1fe015b7f7fc12932
Hafizh Adwinsyah <hafizh.adwinsyah@gmail.com>
2022-07-26 12:37:06 +0700

    add notification store

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 import { API_URL, API_KEY } from "@/config"
+import { useNotificationStore } from "@/stores/notification";
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
@@ -10,5 +11,17 @@ export const axiosInstance = axios.create({
 })
 
 // axiosInstance.interceptors.request.use((config: AxiosRequestConfig) => {
-
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response.data
+  },
+  (error) => {
+    const message = error.response.data.message || error.message
+    useNotificationStore.getState().addNotification({
+      type: 'error',
+      title: 'Error',
+      message: message
+    })
+  }
+)
 // })
