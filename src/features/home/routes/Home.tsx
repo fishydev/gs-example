c18cd16ba68c6ac9ba628ba62381e4226e119d0d
Hafizh Adwinsyah <hafizh.adwinsyah@gmail.com>
2022-07-26 15:01:26 +0700

    home update

--- src/features/home/routes/Home.tsx
+++ src/features/home/routes/Home.tsx
@@ -1,7 +1,12 @@
+import { Banner } from "../components/Banner"
+import { Popular } from "../components/Popular"
+
 export const Home = () => {
   return (
     <div className="w-full">
-      <h1>Home page</h1>
+      {/* <h1>Home page</h1> */}
+      <Banner />
+      <Popular />
     </div>
   )
 }
