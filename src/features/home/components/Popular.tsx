794fb63d1195d852b10b43599a7c2736038fc8ce
Hafizh Adwinsyah <hafizh.adwinsyah@gmail.com>
2022-07-27 15:29:22 +0700

    fix error

--- src/features/home/components/Popular.tsx
+++ src/features/home/components/Popular.tsx
@@ -1,6 +1,6 @@
 import { getPopular } from "../api/getPopular"
 
-import { MovieCard } from "@/components/Elements/MovieCard"
+import { ItemCard } from "@/components/Elements/ItemCard"
 
 const movies = getPopular()
 
@@ -9,8 +9,8 @@ export const Popular = () => {
     <div className="flex-col p-8">
       <p className="text-xl mb-4">What's Popular</p>
       <div className="flex space-x-4 overflow-x-scroll">
-        {movies.map((movie) => (
-          <MovieCard {...movie} />
+        {movies.map((movie, index) => (
+          <ItemCard {...movie} key={index} />
         ))}
       </div>
     </div>
