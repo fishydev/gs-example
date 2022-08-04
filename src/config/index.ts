223d101d4e1f6ad89e83b12c5688869ec2f5674a
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-08-04 10:47:41 +0700

    update config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_URL as string
-export const API_KEY = process.env.REACT_APP_API_KEY as string
+export const API_URL = "https://api.themoviedb.org/3"
+export const API_KEY = "9b86a6374d8c8d46fa64417e116069ae"
