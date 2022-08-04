e79a42e62b29338890424426a3da66faf31f5a0f
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-08-04 10:48:42 +0700

    axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,13 +1,13 @@
-import axios from "axios";
+import axios from "axios"
 
 import { API_URL, API_KEY } from "@/config"
-import { useNotificationStore } from "@/stores/notification";
+import { useNotificationStore } from "@/stores/notification"
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
   params: {
-    api_key: API_KEY
-  }
+    api_key: API_KEY,
+  },
 })
 
 // axiosInstance.interceptors.request.use((config: AxiosRequestConfig) => {
@@ -18,10 +18,12 @@ axiosInstance.interceptors.response.use(
   (error) => {
     const message = error.response.data.message || error.message
     useNotificationStore.getState().addNotification({
-      type: 'error',
-      title: 'Error',
-      message: message
+      type: "error",
+      title: "Error",
+      message,
     })
+
+    return Promise.reject(error)
   }
 )
 // })
