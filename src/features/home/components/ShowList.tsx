4b7ae26da2498328ca59a90fe87bd9378f468d7d
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-08-05 11:34:52 +0700

    add show list component

---
+++ src/features/home/components/ShowList.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom"
+import { Card } from "@/components/Elements/Card"
+import { MovieCardContent } from "./MovieCardContent"
+import { Movie } from "@/features/movies/types"
+import { Tv } from "@/features/tv/types"
+
+type ShowListProps = {
+  items?: Movie[] | Tv[]
+  isLoading: boolean
+}
+
+export const ShowList = (props: ShowListProps) => {
+  const { isLoading, items } = props
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if (!items) {
+    return <p>No Data Found</p>
+  }
+
+  return (
+    <div className="flex space-x-4 overflow-x-scroll h-[400px]">
+      {items.map((show) => (
+        <Link
+          to={`/${show.media_type === "tv" ? "tv" : "movies"}/${show.id}`}
+          key={show.id}
+        >
+          <Card>
+            <MovieCardContent {...show} />
+          </Card>
+        </Link>
+      ))}
+    </div>
+  )
+}
