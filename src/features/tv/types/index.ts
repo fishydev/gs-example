bf1987e91737fdfb9c2c70ceeddfd04e949af0c2
Hafizh Adwinsyah <huvuhuvuhuvu@gmail.com>
2022-08-06 13:18:31 +0700

    add tv types

--- src/features/tv/types/index.ts
+++ src/features/tv/types/index.ts
@@ -32,3 +32,36 @@ export type TvCredits = {
   id: number
   cast: Cast[]
 }
+
+export type TvDetail = {
+  backdrop_path: string | null
+  first_air_date: string
+  genres: TvGenre[]
+  homepage: string
+  id: number
+  in_production: boolean
+  languages: string[]
+  last_air_date: string
+  name: string
+  number_of_episodes: number
+  number_of_seasons: number
+  overview: string
+  popularity: number
+  poster_path: string | null
+  seasons: TvSeason[]
+  status: string
+  tagline: string | null
+  type: string
+  vote_average: number
+  vote_count: number
+}
+
+export type TvSeason = {
+  air_date: Date
+  episode_count: number
+  id: number
+  name: string
+  overview: string
+  poster_path: string
+  season_number: number
+}
